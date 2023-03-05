dde1944793f7855b65467db6837643d02b04493b
JLD986 <126208860+JLD986@users.noreply.github.com>
2023-03-05 17:28:11 -0600

    Add files via upload

---
+++ actividadAsincrona – Semana06(Grupo).py
@@ -0,0 +1,118 @@
+print ("Suma de tres variables")
+print ("Primer variable")
+numeroUno = 12
+numeroDos = 48
+print ("La suma de la primer variable es:")
+print (numeroUno + numeroDos)
+
+print ("Segunda variable")
+numeroTres = 30
+numeroCuatro = 20
+print ("La suma de la segunda variable es:")
+print (numeroTres + numeroCuatro)
+
+print ("Tercer variable")
+numeroCinco = 70
+numeroSeis = 30
+print ("La suma de la tercera variable es:")
+print (numeroCinco + numeroSeis)
+
+
+#Resta de cuatro variables
+print ("Resta de cuatro variables")
+print ("Primer variable")
+numeroSiete = 48
+numeroOcho = 8
+print ("La resta de la primer variable es:")
+print (numeroSiete - numeroOcho)
+
+print ("Segunda variable")
+numeroNueve = 25
+numeroDiez = 15
+print ("La resta de la segunda variable es:")
+print (numeroNueve - numeroDiez)
+
+print ("Tercera variable")
+numeroOnce = 40
+numeroDoce = 28
+print ("La resta de la tercera variable es:")
+print (numeroOnce - numeroDoce)
+
+print ("Cuarta variabale")
+numeroTrece = 120
+numeroCatorce = 40
+print ("La resta de la cuarta variable es:")
+print (numeroTrece - numeroCatorce)
+
+
+#Multiplicación de dos variables
+print ("Multiplicación de dos variables")
+print ("Primer variable")
+numeroQuince = 2
+numeroDieciséis = 4
+print ("La multiplicación de la primer variable es:")
+print (numeroQuince * numeroDieciséis)
+
+print ("Segunda variable")
+numeroDiecisiete = 3
+numeroDieciocho = 2
+print ("La multiplicación de la segunda variable es:")
+print (numeroDiecisiete * numeroDieciocho)
+
+
+print ("Multiplicación por segunda variable de suma y resta")
+print ("Multiplicación por segunda variable de la suma")
+numeroDiecinueve = 8
+numeroVeinte = 50
+
+numeroVeintiuno = 6
+numeroVeintidos = 50
+print ("La multiplicación de la segunda variable de la suma por las dos anteriores multiplicaciones es:")
+print (numeroDiecinueve * numeroVeinte)
+print (numeroVeintiuno * numeroVeintidos)
+
+print ("Multiplicación por la segunda variable de la resta")
+numeroVeintitres = 8
+numeroVeinticuatro = 10
+
+numeroVeinticinco = 6
+numeroVeintiséis = 10
+print ("La multiplicación de la segunda variable de la resta por las dos anteriores multiplicaciones es:")
+print (numeroVeintitres * numeroVeinticuatro)
+print (numeroVeinticinco * numeroVeintiséis)
+
+
+#Exponente de una variable
+print ("Exponente de una variable")
+print ("Exponente de la primera variable de la multiplicación con la segunda variable")
+numeroVeintisiete = 8
+numeroVeintiocho = 6
+print ("El exponenete de la primera variable de la multiplicación con la segunda es:")
+print (numeroVeintisiete ** numeroVeintiocho)
+
+
+#Modulo de una variable
+print ("Modulo de una variable")
+print ("Modulo entre la primera variable de la suma y la primera variable de la resta")
+numeroVeintinueve = 60
+numeroTreinta = 40
+print ("El modulo entre la primera variable de la suma y la primera variable de la resta es:")
+print (numeroVeintinueve % numeroTreinta)
+
+
+#División de una variable
+print ("División de una variable")
+print ("Dividir la variable de exponente entre la variable del modulo")
+numeroTreinta_y_uno = 262144
+numeroTreinta_y_dos = 20
+print ("El resultado de la división entre la variable de exponente entre la variable del modulo es:")
+print (numeroTreinta_y_uno / numeroTreinta_y_dos)
+
+
+#División entera de una variable
+print ("División entera de una variable")
+print ("División entera de la variable de exponente entre la variable del modulo")
+numeroTreinta_y_tres = 262144
+numeroTreinta_y_cuatro = 20
+print ("El resultado de la división entera entre la variable de exponente entre la variable del modulo es:")
+print (numeroTreinta_y_tres // numeroTreinta_y_cuatro)
